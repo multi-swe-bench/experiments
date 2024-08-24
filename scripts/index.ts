650f708859268a64892e77dba42a7586532ff75d
multi-swe-bench <elidupree@proton.me>
2024-08-24 15:39:55 +0800

    add build script

---
+++ scripts/index.ts
@@ -0,0 +1,84 @@
+import * as yaml from 'js-yaml'
+import * as fs from 'node:fs/promises'
+
+const GITHUB_URL = 'https://github.com/multi-swe-bench/experiments/tree/main/evaluation'
+
+const index = yaml.load(await fs.readFile('index.yaml', 'utf8')) as Record<string, {
+  name: string
+  data?: Record<string, {
+    name: string
+    total: number
+  }>
+}>
+
+interface ListItem<T> {
+  name: string
+  data?: T[]
+}
+
+interface Result {
+  no_generation: string[]
+  generated: string[]
+  with_logs: string[]
+  install_fail: string[]
+  reset_failed: string[]
+  no_apply: string[]
+  applied: string[]
+  test_errored: string[]
+  test_timerout: string[]
+  resolved: string[]
+}
+
+interface Data {
+  oss: boolean
+  verified: boolean
+  name: string
+  resolved: number
+  date: string
+  logs?: string
+  trajs?: string
+  site: string
+}
+
+const leaderboard: ListItem<ListItem<Data>>[] = []
+
+for (const [k1, v1] of Object.entries(index)) {
+  const item: ListItem<ListItem<Data>> = {
+    name: v1.name,
+    data: v1.data && [],
+  }
+  leaderboard.push(item)
+  if (!v1.data) continue
+  for (const [k2, v2] of Object.entries(v1.data)) {
+    const dirents = await fs.readdir(`evaluation/${k1}/${k2}`, { withFileTypes: true })
+    const data = await Promise.all(dirents
+      .filter((dirent) => dirent.isDirectory() && !dirent.name.startsWith('.'))
+      .map<Promise<Data>>(async (dirent) => {
+        const results = JSON.parse(await fs.readFile(`evaluation/${k1}/${k2}/${dirent.name}/results/results.json`, 'utf8')) as Result
+        const metadata = yaml.load(await fs.readFile(`evaluation/${k1}/${k2}/${dirent.name}/metadata.yaml`, 'utf8')) as Pick<Data, 'oss' | 'verified' | 'name' | 'site'>
+        const date = dirent.name.split('_', 1)[0]
+        const urlLogs = `${GITHUB_URL}/${k1}/${k2}/${dirent.name}/logs`
+        const urlTrajs = `${GITHUB_URL}/${k1}/${k2}/${dirent.name}/trajs`
+        const hasLogs = await fs.access(`evaluation/${k1}/${k2}/${dirent.name}/logs`).then(() => true, () => false)
+        const hasTrajs = await fs.access(`evaluation/${k1}/${k2}/${dirent.name}/trajs`).then(() => true, () => false)
+        return {
+          name: metadata.name,
+          site: metadata.site,
+          oss: metadata.oss,
+          verified: metadata.verified,
+          resolved: results.resolved.length * 100 / v2.total,
+          date: `${date.slice(0, 4)}-${date.slice(4, 6)}-${date.slice(6)}`,
+          logs: hasLogs ? urlLogs : undefined,
+          trajs: hasTrajs ? urlTrajs : undefined,
+        }
+      }))
+    data.sort((a, b) => b.resolved - a.resolved)
+    item.data!.push({
+      name: v2.name,
+      data: data,
+    })
+  }
+}
+
+await fs.mkdir('dist', { recursive: true })
+await fs.writeFile('dist/leaderboard.json', JSON.stringify(leaderboard, null, 2))
